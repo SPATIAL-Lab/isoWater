4b18377d22e57948daadc02e47923709b527aa1a
Gabriel Bowen <gabe.bowen@utah.edu>
2021-03-04 17:51:31 -0700

    Add parallel option

--- watercomp.R
+++ watercomp.R
@@ -21,55 +21,13 @@ iso = function(H,O,Hsd,Osd,HOc){
 }
 
 #####
-#--- single source implementation ----
-#####
-
-#takes values of observed and hypothesized source water (each type 'iso'), hypothesized EL slope value
-#prior probability of source, and number of parameter draws
-sourceprob = function(obs, hsource, hslope, prior=1, ngens=10000){
-  
-  #ngens observed values
-  HO_obs = rmvnorm(ngens, c(obs$H, obs$O), matrix(c(obs$Hsd^2, rep(obs$HOc*obs$Hsd*obs$Osd,2), obs$Osd^2),2,2))
-  H_obs = HO_obs[,1]
-  O_obs = HO_obs[,2]
-  obs_prob = vector(,ngens)
-  for(i in 1:ngens){
-    obs_prob[i] = dmvnorm(c(H_obs[i], O_obs[i]),c(obs$H, obs$O), sigma=matrix(c(obs$Hsd^2, rep(obs$HOc*obs$Hsd*obs$Osd,2), obs$Osd^2),2,2))
-  }
-  
-  #ngens hypothesized source values
-  HO_h = rmvnorm(ngens, c(hsource$H, hsource$O), matrix(c(hsource$Hsd^2, rep(hsource$HOc*hsource$Hsd*hsource$Osd,2), hsource$Osd^2),2,2))
-  H_h=HO_h[,1]
-  O_h=HO_h[,2]
-  hypo_prob = vector(,ngens)
-  for(i in 1:ngens){
-    hypo_prob[i] = dmvnorm(c(H_h[i], O_h[i]),c(hsource$H, hsource$O), sigma=matrix(c(hsource$Hsd^2, rep(hsource$HOc*hsource$Hsd*hsource$Osd,2), hsource$Osd^2),2,2))
-  }
-  
-  #get slope and standardized source probability for each case
-  S = (H_obs-H_h)/(O_obs-O_h)
-  Sprob = dnorm(S, hslope[1], hslope[2])/dnorm(hslope[1], hslope[1], hslope[2])
-  
-  #if sample value lies below or left of source evaporation can't explain it
-  msk = ifelse(H_h > H_obs | O_h > O_obs, 0, 1)
-  Sprob = Sprob * msk
-  goods = sum(msk)
-
-  results = data.frame(H_h, O_h, hypo_prob, H_obs, O_obs, obs_prob, Sprob) #return data frame w/ all draws
-
-  print(paste(goods, "out of", ngens))  #how many non-zero probabilities?
-
-  return(results)
-  
-}
-
-#####
 #--- MWL source implementation ----
 #####
 
 #takes values of observed water (type 'iso'), MWL (see below), hypothesized EL slope value
 #and number of parameter draws
-mwlsource = function(obs, mwl=c(8.01, 9.57, 167217291.1, 2564532.2, -8.096, 80672), hslope, ngens=10000){
+mwlsource = function(obs, mwl=c(8.01, 9.57, 167217291.1, 2564532.2, -8.096, 80672), 
+                     hslope, ngens=1e4, ncores = 1){
   #mwl contains parameters for the meteoric water line:
   #slope, intercept, sum of squares in d2H, sum of squares in d18O, average d18O, and number of samples.
   #Defaults were calculated from precipitation samples extracted from waterisotopes.org DB on 7/7/2017,
@@ -109,8 +67,13 @@ mwlsource = function(obs, mwl=c(8.01, 9.57, 167217291.1, 2564532.2, -8.096, 8067
   n.iter = ngens * 1.1
   n.burnin = ngens * 0.1
   n.thin = floor((n.iter - n.burnin) / 2500)
-  post = jags(d, NULL, p, lmwl, n.iter = n.iter, 
-              n.burnin = n.burnin, n.thin = n.thin)
+  if(ncores > 1){
+    post = jags.parallel(d, NULL, p, lmwl, n.iter = n.iter, 
+              n.burnin = n.burnin, n.thin = n.thin)    
+  } else{
+    post = jags(d, NULL, p, lmwl, n.iter = n.iter, 
+              n.burnin = n.burnin, n.thin = n.thin)    
+  }
 
   sl = post$BUGSoutput$sims.list
   results = data.frame(sl$h_s, sl$o_s, sl$slp, sl$evap)
@@ -126,7 +89,8 @@ mwlsource = function(obs, mwl=c(8.01, 9.57, 167217291.1, 2564532.2, -8.096, 8067
 
 #takes values of observed and hypothesized endmember source waters (each type 'iso'),hypothesized EL slope,
 #prior (as relative contribution of each source to mixture), and number of parameter draws
-mixprob = function(obs, hsource, hslope, prior=rep(1,nrow(hsource)), shp=2, ngens=100000){
+mixprob = function(obs, hsource, hslope, prior=rep(1,nrow(hsource)), 
+                   shp=2, ngens=1e5, ncores = 1){
 
   #get number of observations
   nobs = nrow(obs)
@@ -165,20 +129,26 @@ mixprob = function(obs, hsource, hslope, prior=rep(1,nrow(hsource)), shp=2, ngen
            alphas = alphas, hslope = hslope)
   
   #parameters
-  p = c("h_s", "fracs", "slp", "evap")
+  p = c("mix.h", "mix.o", "fracs", "slp", "evap")
 
   #runit
   n.iter = ngens * 1.1
   n.burnin = ngens * 0.1
   n.thin = floor((n.iter - n.burnin) / 2500)
-  post = jags(d, NULL, p, mixmod, n.iter = n.iter, 
-              n.burnin = n.burnin, n.thin = n.thin)
-  
+  if(ncores > 1){
+    post = jags.parallel(d, NULL, p, mixmod, n.iter = n.iter, 
+                n.burnin = n.burnin, n.thin = n.thin)
+  } else{
+    post = jags(d, NULL, p, mixmod, n.iter = n.iter, 
+                n.burnin = n.burnin, n.thin = n.thin)
+    
+  }
+
   sl = post$BUGSoutput$sims.list
-  results = data.frame(sl$fracs, sl$slp, sl$evap)
+  results = data.frame(sl$mix.h, sl$mix.o, sl$fracs, sl$slp, sl$evap)
   #reassign names to results dataframe
-  n = "s1_fraction"
-  for(i in 2:nsource){
+  n = c("mixture_d2H", "mixture_d18O")
+  for(i in 1:nsource){
     nadd = paste0("s", i, "_fraction")
     n = c(n, nadd)
   }
@@ -191,8 +161,8 @@ mixprob = function(obs, hsource, hslope, prior=rep(1,nrow(hsource)), shp=2, ngen
 mixmod = function(){
   #Data model
   for(i in 1:nobs){
-    obs[i, 1:2] ~ dmnorm.vcov(c(h_pred, o_pred), obs.vcov[(1 + (i-1) * 2):
-                                                        (2 + (i-1) * 2),])
+    obs[i, 1:2] ~ dmnorm.vcov(c(h_pred, o_pred), 
+                              obs.vcov[(1 + (i-1) * 2):(2 + (i-1) * 2),])
   }
   
   #Process model
@@ -212,16 +182,16 @@ mixmod = function(){
   
   #sources prior
   for(i in 1:nsource){
-    h_s[i, 1:2] ~ dmnorm.vcov(s[i, 1:2], s.vcov[(1 + (i-1) * 2):
-                                                  (2 + (i-1) * 2),])
+    h_s[i, 1:2] ~ dmnorm.vcov(s[i, 1:2], 
+                              s.vcov[(1 + (i-1) * 2):(2 + (i-1) * 2),])
   }
 }
 
 lmwl = function(){
   #Data model
   for(i in 1:nobs){
-    obs[i, 1:2] ~ dmnorm.vcov(c(h_pred, o_pred), obs.vcov[(1 + (i-1) * 2):
-                                                        (2 + (i-1) * 2),])
+    obs[i, 1:2] ~ dmnorm.vcov(c(h_pred, o_pred), 
+                              obs.vcov[(1 + (i-1) * 2):(2 + (i-1) * 2),])
   }
   
   #Process model
@@ -237,5 +207,5 @@ lmwl = function(){
   #MWL source prior
   h_s ~ dnorm(o_s * mwl[1] + mwl[2], 1 / sy ^ 2) 
   sy = sr * sqrt(1 + 1 / mwl[6] + (o_s - mwl[5])^2 / mwl[4])
-  o_s ~ dunif(o_min, o_max)
+  o_s ~ dnorm(o_cent, 1 / o_var)
 }
